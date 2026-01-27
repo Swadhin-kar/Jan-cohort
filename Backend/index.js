b33203e368790e35874ae603eba478d22a3a95d4
Swadhin-kar <swadhinkarjan26@gmail.com>
2026-01-27 22:55:13 +0530

    Job description connected to backend

--- Backend/index.js
+++ Backend/index.js
@@ -28,5 +28,5 @@ app.use('/ai', aiRouter)
 const PORT = process.env.PORT || 7000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  // console.log(`Server running on port ${PORT}`)
 })
